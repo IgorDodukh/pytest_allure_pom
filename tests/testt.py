51e0be358e3116f6e81776eed2ba0badcad0d301
IgorDodukh <ihor.dodukh@gmail.com>
2017-09-14 17:56:38 +0300

    update test

--- tests/testt.py
+++ tests/testt.py
@@ -1,19 +1,46 @@
-import allure
+# import allure
+#
+#
+# @allure.severity(allure.severity_level.MINOR)
+# def test_minor():
+#     assert False
+#
+#
+# @allure.severity(allure.severity_level.CRITICAL)
+# class TestBar:
+#
+#     # will have CRITICAL priority
+#     def test_bar(self):
+#         pass
+#
+#     # will have BLOCKER priority via a short-cut decorator
+#     @allure.severity(allure.severity_level.CRITICAL)
+#     def test_bar(self):
+#         pass
+
+import pytest
 
 
-@allure.severity(allure.severity_level.MINOR)
-def test_minor():
-    assert False
+@pytest.fixture(scope="function")
+def resource_setup(request):
+    print("\nconnect to db")
+    db = {"Red": 1, "Blue": 2, "Green": 3}
+
+    def resource_teardown():
+        print("\ndisconnect")
+
+    request.addfinalizer(resource_teardown)
+    return db
 
 
-@allure.severity(allure.severity_level.CRITICAL)
-class TestBar:
+def test_db(resource_setup):
+    for k in resource_setup.keys():
+        print("color {0} has id {1}".format(k, resource_setup[k]))
 
-    # will have CRITICAL priority
-    def test_bar(self):
-        pass
 
-    # will have BLOCKER priority via a short-cut decorator
-    @allure.severity(allure.severity_level.CRITICAL)
-    def test_bar(self):
-        pass
+def test_red(resource_setup):
+    assert resource_setup["Red"] == 1
+
+
+def test_blue(resource_setup):
+    assert resource_setup["Blue"] != 1
